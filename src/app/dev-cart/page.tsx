90bc1a00c7f0163792ea5a6f291a9232cceb5c09
Enockii <enocki.9@outlook.fr>
2023-09-26 14:52:09 +0200

    TP2 - 1.2 - Panier dans dev-cart

--- src/app/dev-cart/page.tsx
+++ src/app/dev-cart/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
-import { Button, ProductCardLayout, SectionContainer } from "tp-kit/components";
+import {Button, ProductCardLayout, ProductCartLine, SectionContainer} from "tp-kit/components";
 const products = PRODUCTS_CATEGORY_DATA[0].products.slice(0, 3);
 
 export default function DevCartPage() {
@@ -23,9 +23,22 @@ export default function DevCartPage() {
             {/* /Produits */}
 
             {/* Panier */}
-            <section className="w-full lg:w-1/3 space-y-8">
-
-                <Button variant={"outline"} fullWidth>Vider le panier</Button>
+            <section className="w-full lg:w-1/3 space-y-8 bg-white p-5 rounded-lg shadow-2xl flex flex-col">
+                <span className={"font-bold uppercase"}>Mon panier</span>
+                    {products.map((product) => (
+                        <ProductCartLine
+                            key={product.id}
+                            onDelete={function noRefCheck() {} }
+                            onQtyChange={function noRefCheck() {} }
+                            product={product}
+                            qty={1}
+                        /> ))
+                    }
+                    <div className={"flex justify-between"}>
+                        <span className={"font-bold"}>Total</span>
+                        <span className={"font-bold"}>Blabla€</span>
+                    </div>
+                <Button className={"text-white border-none bg-green"} variant={"outline"} fullWidth>Commander</Button>
             </section>
             {/* /Panier */}
         </SectionContainer>
