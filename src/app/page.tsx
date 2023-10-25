1ccc819c4ae05cd987fc9388c4b5434db5dc8ef2
Enockii <enocki.9@outlook.fr>
2023-10-25 16:24:01 +0200

    TP3 - 3.2 - Affichage de la page d'accueil dynamique

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,30 @@
 import {BreadCrumbs, Footer} from "tp-kit/components";
 import ProductList from "@/components/productList";
 
-import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
+import {PRODUCTS_CATEGORY_DATA} from "tp-kit/data";
 import {Metadata} from "next";
-const categories = PRODUCTS_CATEGORY_DATA;
+import prisma from "@/utils/prisma";
 
 export const metadata: Metadata = {
     description: "Commandez de délicieuses boissons préparées avec sois par nos baristas"
 }
 
-export default function Home() {
-  return (
-      <main>
-          <BreadCrumbs  className={"p-10 px-40 bg-grayPicture"} items={[
-              {
-                  "label": "Accueil",
-                  "url": "/"
-              }
-          ]}></BreadCrumbs>
-          <ProductList categoriesToDisplay={categories} showFilters={true} />
+export default async function Home() {
+    const categories = await prisma.productCategory.findMany({
+        include: {
+            products: {}
+        }
+    });
+    return (
+        <main>
+            <BreadCrumbs className={"p-10 px-40 bg-grayPicture"} items={[
+                {
+                    "label": "Accueil",
+                    "url": "/"
+                }
+            ]}></BreadCrumbs>
+            <ProductList categoriesToDisplay={categories} showFilters={true}/>
 
-    </main>
-  )
+        </main>
+    )
 }
