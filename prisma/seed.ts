88275ad6672328014a2ade4122da2d6db60e65f9
Enockii <enocki.9@outlook.fr>
2023-10-25 15:46:17 +0200

    TP3 - 2.3 - Seed prisma

---
+++ prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from '@prisma/client'
+import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
+
+const prisma = new PrismaClient()
+async function main() {
+    for (const productCategories of PRODUCTS_CATEGORY_DATA) {
+
+        /*const productCategoriesToCreate = await prisma.productCategory.create({
+            data: {
+                id: productCategories.id,
+                slug: productCategories.slug,
+                name: productCategories.name
+            },
+        })*/
+
+        const products = productCategories.products;
+        for (const product of products) {
+            const productsToCreate = await prisma.product.upsert({
+                where: {id: product.id},
+                update: { desc: product.desc },
+                create: {
+                    id: product.id,
+                    slug: product.slug,
+                    name: product.name,
+                    img: product.img,
+                    path: product.path,
+                    price: product.price,
+                    desc: product.desc,
+                    categoryId: productCategories.id
+                }
+            })
+        }
+
+        /*const products = productCategories.products;
+        for (const product of products) {
+            const productsToCreate = await prisma.product.create({
+               data: {
+                       id: product.id,
+                       slug: product.slug,
+                       name: product.name,
+                       img: product.img,
+                       path: product.path,
+                       price: product.price,
+                       desc: product.desc,
+                       categoryId: productCategories.id
+                   }
+            })
+        }*/
+    }
+
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
